e66d8ff6698ec837c5b1fe136f050178e5b79299
zhonghes <114350562+zhonghes@users.noreply.github.com>
2025-09-16 23:46:10 +0800

    Update push_images.sh

--- accr/push_images.sh
+++ accr/push_images.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
+# 格式一：
+#     平台,镜像,前缀
+# 格式二：
+#   镜像
 LIST_IMAGES=(
     "redis:6"
+    "linux/arm64/v8,redis:6,aarch64"
     "redis:7"
     "outlinewiki/outline:latest"
 )
 
 function Main() {
-    local _tag _new_tag
+    local _platform _prefix _tag _new_tag
     
     for image in "${LIST_IMAGES[@]}"; do
-        _tag="${image#*,}"
-        _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_tag##*/}"
+        if echo "${image}" | awk -F ',' '{print NF}' | grep -q -s '3'; then
+            _platform=$(echo "${image}" | awk -F ',' '{print $1}')
+            _tag=$(echo "${image}" | awk -F ',' '{print $2}')
+            _prefix=$(echo "${image}" | awk -F ',' '{print $3}')
+
+            _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_prefix}_${_tag##*/}"
+        else
+            _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_tag##*/}"
+        fi
         
         docker pull "${_tag}"
         docker tag "${_tag}" "${_new_tag}"
