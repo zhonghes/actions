cda857249c9c6a2cee7ee6f3e44a5a5dbad7287e
zhonghes <114350562+zhonghes@users.noreply.github.com>
2025-09-16 22:57:23 +0800

    Update push_images.sh

--- accr/push_images.sh
+++ accr/push_images.sh
@@ -8,10 +8,9 @@ function Main() {
   local _tag _new_tag
 
   for image in "${LIST_IMAGES[@]}": do
-    # _platform="${image%,*}"
     _tag="${image#*,}"
     _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_tag}"
-    # docker pull --platform="${_platform}" "${_tag}"
+    
     docker pull "${_tag}"
     docker tag "${_tag}" "${_new_tag}"
     docker push "${_new_tag}"
