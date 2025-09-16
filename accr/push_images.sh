373dde577e77cd2aab79d42e55894d84b6a195f4
zhonghes <114350562+zhonghes@users.noreply.github.com>
2025-09-16 23:00:54 +0800

    Update push_images.sh

--- accr/push_images.sh
+++ accr/push_images.sh
@@ -1,20 +1,20 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 LIST_IMAGES=(
     "redis:7"
-  )
+)
 
 function Main() {
-  local _tag _new_tag
-
-  for image in "${LIST_IMAGES[@]}": do
-    _tag="${image#*,}"
-    _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_tag}"
+    local _tag _new_tag
     
-    docker pull "${_tag}"
-    docker tag "${_tag}" "${_new_tag}"
-    docker push "${_new_tag}"
-  done
+    for image in "${LIST_IMAGES[@]}"; do
+        _tag="${image#*,}"
+        _new_tag="crpi-qqjr4xowoqr9zwh0.cn-beijing.personal.cr.aliyuncs.com/zhonghes/${_tag}"
+        
+        docker pull "${_tag}"
+        docker tag "${_tag}" "${_new_tag}"
+        docker push "${_new_tag}"
+    done
 }
 
 Main "$@"
